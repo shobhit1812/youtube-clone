42d8f9948fd759a06a59ee22ca3d3b32c58a6784
Shobhit Nautiyal <shobhit.nautiyal99@gmail.com>
2024-07-08 16:07:40 +0530

    https

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -5,6 +5,6 @@ export const YOUTUBE_VIDEO_API =
   GOOGLE_API_KEY;
 
 export const YOUTUBE_SEARCH_API =
-  "http://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q=";
+  "https://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q=";
 
 export const OFFSET_LIVE_CHAT = 75;
